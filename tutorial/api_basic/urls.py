aef3b25b87ce8218b9eda49456a5e7b5fce47e8f
rahulkranjan <kjrahul21@gmail.com>
2020-06-22 23:59:03 +0530

    first rest commit

---
+++ tutorial/api_basic/urls.py
@@ -0,0 +1,22 @@
+
+from django.urls import path,include
+from .views import ArticleAPIView, ArticleDetails,GenericAPIView,ArticleViewSet
+from rest_framework.routers import DefaultRouter
+from rest_framework_jwt.views import obtain_jwt_token
+
+
+router = DefaultRouter()
+router.register('article', ArticleViewSet, basename='article')
+
+urlpatterns = [
+ 
+    path('article/', ArticleAPIView.as_view()),
+    path('detail/<int:id>/', ArticleDetails.as_view()),
+    path('generic/article/<int:id>/', GenericAPIView.as_view()),
+    path('viewset/', include(router.urls)),
+    path('viewset/<int:id>/', include(router.urls)),
+    path('api-token-auth/', obtain_jwt_token),
+
+ 
+ 
+]
